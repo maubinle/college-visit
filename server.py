a547b967bd86cfbc678cb82ef8fa582c61b072b7
canovalentina <valentina_cano@brown.edu>
2016-02-06 16:54:59 -0500

    change server

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import abort, redirect, url_for
+
 
 app = Flask(__name__)
 
@@ -8,13 +11,16 @@ def hello():
     return render_template('index.html')
 
 @app.route('/')
+def style():
+	return url_for('static', filename='style.css')
+
+@app.route('/')
 def hello_world():
     return 'Hello World!'
 
 @app.route('/test/')
 def test_page():
 	return "Page 2"
-
 
 
 if __name__ == '__main__':
