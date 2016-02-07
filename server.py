7a6481311e2335836593afda20558d489a330a5a
canovalentina <valentina_cano@brown.edu>
2016-02-06 19:58:35 -0500

    yo

--- server.py
+++ server.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import abort, redirect, url_for
+import json
+import urllib2
+
 
 
 app = Flask(__name__)
@@ -10,9 +13,13 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
-@app.route('/')
-def style():
-	return url_for('static', filename='style.css')
+@app.route('/test')
+def get_json(): 
+	response = urllib2.urlopen('http://nearbycolleges.info/api/autocomplete?q=university%20of%20i&limit=3')
+	json_string = response.read()
+	parsed_json = json.loads(json_string)
+	print json_string
+
 
 @app.route('/')
 def hello_world():
@@ -21,6 +28,9 @@ def hello_world():
 @app.route('/test/')
 def test_page():
 	return "Page 2"
+
+
+
 
 
 if __name__ == '__main__':
